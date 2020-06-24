ae18d05af416cbe7491c7d6e2a2469bb488fde8b
Wonmin Jeong <wonmin11@gmail.com>
2020-06-24 23:41:53 +0000

    Update ShowIexQuote

--- springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/AppConfig.java
@@ -13,5 +13,9 @@ public class AppConfig {
 
     @Bean
     public MarketDataConfig marketDataConfig() {
+        MarketDataConfig marketDataConfig = new MarketDataConfig();
+        marketDataConfig.setHost("https://cloud.iexapis.com/v1/");
+        marketDataConfig.setToken(System.getenv("IEX_PUB_TOKEN"));
+        return marketDataConfig;
     }
 }
